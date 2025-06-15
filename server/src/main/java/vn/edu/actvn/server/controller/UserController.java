4364f975896d0c1e32a40366a3aa2b9393af65dc
PhongPKF <ntphong2k4cb@gmail.com>
2025-06-16 01:37:09 +0700

    fix

--- server/src/main/java/vn/edu/actvn/server/controller/UserController.java
+++ server/src/main/java/vn/edu/actvn/server/controller/UserController.java
@@ -30,119 +30,117 @@ import vn.edu.actvn.server.service.UserService;
 @Slf4j
 @Tag(name = "User API", description = "Endpoints for user management and information")
 public class UserController {
-    UserService userService;
+        UserService userService;
 
-    @PostMapping
-    @Operation(summary = "Create a new user")
-    public ApiResponse<UserResponse> createUser(@RequestBody @Valid CreateAdminRequest request) {
-        return ApiResponse.<UserResponse>builder()
-                .result(userService.createUser(request))
-                .message("User created successfully")
-                .build();
-    }
+        @PostMapping
+        @Operation(summary = "Create a new user")
+        public ApiResponse<UserResponse> createUser(@RequestBody @Valid CreateAdminRequest request) {
+                return ApiResponse.<UserResponse>builder()
+                                .result(userService.createUser(request))
+                                .message("User created successfully")
+                                .build();
+        }
 
-    @GetMapping
-    @Operation(summary = "Get all users")
-    public ApiResponse<Page<UserResponse>> getUsers(
-            @RequestParam(value = "page", defaultValue = "0") int page,
-            @RequestParam(value = "size", defaultValue = "10") int size,
-            @RequestParam(value = "sort", defaultValue = "username,asc") String sort
-    ) {
-        Sort pageSort;
-        String[] sortPart = sort.split(",");
-        String direction = sortPart.length > 1 ? sortPart[1] : "asc";
-        String sortField = sortPart[0].trim();
-        Sort.Direction sortDirection = Sort.Direction.fromString(direction.toUpperCase());
-        String actualSortField = "fullName".equalsIgnoreCase(sortField) ? "lastName" : sortField;
+        @GetMapping
+        @Operation(summary = "Get all users")
+        public ApiResponse<Page<UserResponse>> getUsers(
+                        @RequestParam(value = "page", defaultValue = "0") int page,
+                        @RequestParam(value = "size", defaultValue = "10") int size,
+                        @RequestParam(value = "sort", defaultValue = "username,asc") String sort) {
+                Sort pageSort;
+                String[] sortPart = sort.split(",");
+                String direction = sortPart.length > 1 ? sortPart[1] : "asc";
+                String sortField = sortPart[0].trim();
+                Sort.Direction sortDirection = Sort.Direction.fromString(direction.toUpperCase());
+                String actualSortField = "fullName".equalsIgnoreCase(sortField) ? "lastName" : sortField;
 
-        pageSort = Sort.by(sortDirection, actualSortField);
+                pageSort = Sort.by(sortDirection, actualSortField);
 
-        Pageable pageable = PageRequest.of(page, size, pageSort);
-        return ApiResponse.<Page<UserResponse>>builder()
-                .result(userService.getUsers(pageable))
-                .message("Fetched all users")
-                .build();
-    }
+                Pageable pageable = PageRequest.of(page, size, pageSort);
+                return ApiResponse.<Page<UserResponse>>builder()
+                                .result(userService.getUsers(pageable))
+                                .message("Fetched all users")
+                                .build();
+        }
 
-    @GetMapping("/role/{roleName}")
-    @Operation(summary = "Get users by role name")
-    public ApiResponse<Page<UserResponse>> getUsersByRoleName(
-            @PathVariable("roleName") String roleName,
-            @RequestParam(value = "page", defaultValue = "0") int page,
-            @RequestParam(value = "size", defaultValue = "10") int size,
-            @RequestParam(value = "sort", defaultValue = "username,asc") String sort
-    ) {
-        Sort pageSort;
-        String[] sortPart = sort.split(",");
-        String direction = sortPart.length > 1 ? sortPart[1] : "asc";
-        String sortField = sortPart[0].trim();
-        Sort.Direction sortDirection = Sort.Direction.fromString(direction.toUpperCase());
-        String actualSortField = "fullName".equalsIgnoreCase(sortField) ? "lastName" : sortField;
+        @GetMapping("/role/{roleName}")
+        @Operation(summary = "Get all users by role name")
+        public ApiResponse<Page<UserResponse>> getUsersByRoleName(
+                        @PathVariable String roleName,
+                        @RequestParam(value = "page", defaultValue = "0") int page,
+                        @RequestParam(value = "size", defaultValue = "10") int size,
+                        @RequestParam(value = "sort", defaultValue = "username,asc") String sort) {
+                Sort pageSort;
+                String[] sortPart = sort.split(",");
+                String direction = sortPart.length > 1 ? sortPart[1] : "asc";
+                String sortField = sortPart[0].trim();
+                Sort.Direction sortDirection = Sort.Direction.fromString(direction.toUpperCase());
+                String actualSortField = "fullName".equalsIgnoreCase(sortField) ? "lastName" : sortField;
 
-        pageSort = Sort.by(sortDirection, actualSortField);
+                pageSort = Sort.by(sortDirection, actualSortField);
 
-        Pageable pageable = PageRequest.of(page, size, pageSort);
-        return ApiResponse.<Page<UserResponse>>builder()
-                .result(userService.getUsersByRole(roleName, pageable))
-                .message("Fetched users by role: " + roleName)
-                .build();
-    }
+                Pageable pageable = PageRequest.of(page, size, pageSort);
+                return ApiResponse.<Page<UserResponse>>builder()
+                                .result(userService.getUsersByRole(roleName, pageable))
+                                .message("Fetched all users by role name")
+                                .build();
+        }
 
-    @GetMapping("/{userId}")
-    @Operation(summary = "Get user by ID")
-    public ApiResponse<UserResponse> getUser(@PathVariable("userId") String userId) {
-        return ApiResponse.<UserResponse>builder()
-                .result(userService.getUser(userId))
-                .message("Fetched user information")
-                .build();
-    }
+        @GetMapping("/{userId}")
+        @Operation(summary = "Get user by ID")
+        public ApiResponse<UserResponse> getUser(@PathVariable("userId") String userId) {
+                return ApiResponse.<UserResponse>builder()
+                                .result(userService.getUser(userId))
+                                .message("Fetched user information")
+                                .build();
+        }
 
-    @GetMapping("/me")
-    @Operation(summary = "Get current logged-in user's information")
-    public ApiResponse<UserResponse> getMyInfo() {
-        return ApiResponse.<UserResponse>builder()
-                .result(userService.getMyInfo())
-                .message("Fetched current user info")
-                .build();
-    }
+        @GetMapping("/me")
+        @Operation(summary = "Get current logged-in user's information")
+        public ApiResponse<UserResponse> getMyInfo() {
+                return ApiResponse.<UserResponse>builder()
+                                .result(userService.getMyInfo())
+                                .message("Fetched current user info")
+                                .build();
+        }
 
-    @DeleteMapping("/{userId}")
-    @Operation(summary = "Delete a user by ID")
-    public ApiResponse<String> deleteUser(@PathVariable String userId) {
-        userService.deleteUser(userId);
-        return ApiResponse.<String>builder()
-                .result("User has been deleted")
-                .message("User deleted successfully")
-                .build();
-    }
+        @DeleteMapping("/{userId}")
+        @Operation(summary = "Delete a user by ID")
+        public ApiResponse<String> deleteUser(@PathVariable String userId) {
+                userService.deleteUser(userId);
+                return ApiResponse.<String>builder()
+                                .result("User has been deleted")
+                                .message("User deleted successfully")
+                                .build();
+        }
 
-    @PutMapping("/{userId}")
-    @Operation(summary = "Update user information")
-    public ApiResponse<UserResponse> updateUser(@PathVariable String userId,
-            @RequestBody UpdateAdminRequest request) {
-        return ApiResponse.<UserResponse>builder()
-                .result(userService.updateUser(userId, request))
-                .message("User updated successfully")
-                .build();
-    }
+        @PutMapping("/{userId}")
+        @Operation(summary = "Update user information")
+        public ApiResponse<UserResponse> updateUser(@PathVariable String userId,
+                        @RequestBody UpdateAdminRequest request) {
+                return ApiResponse.<UserResponse>builder()
+                                .result(userService.updateUser(userId, request))
+                                .message("User updated successfully")
+                                .build();
+        }
 
-    @PatchMapping("/{userId}")
-    @Operation(summary = "Update user information")
-    public ApiResponse<UserResponse> patchUser(@PathVariable String userId,
-            @RequestBody UpdateAdminRequest request) {
-        return ApiResponse.<UserResponse>builder()
-                .result(userService.patchUser(userId, request))
-                .message("User updated successfully")
-                .build();
-    }
+        @PatchMapping("/{userId}")
+        @Operation(summary = "Update user information")
+        public ApiResponse<UserResponse> patchUser(@PathVariable String userId,
+                        @RequestBody UpdateAdminRequest request) {
+                return ApiResponse.<UserResponse>builder()
+                                .result(userService.patchUser(userId, request))
+                                .message("User updated successfully")
+                                .build();
+        }
 
-    @PostMapping("/change-password")
-    @Operation(summary = "Change current user's password")
-    public ApiResponse<String> changePassword(@RequestBody ChangePasswordRequest request) {
-        userService.changePassword(request);
-        return ApiResponse.<String>builder()
-                .result("Password has been changed")
-                .message("Password change successful")
-                .build();
-    }
+        @PostMapping("/change-password")
+        @Operation(summary = "Change current user's password")
+        public ApiResponse<String> changePassword(@RequestBody ChangePasswordRequest request) {
+                userService.changePassword(request);
+                return ApiResponse.<String>builder()
+                                .result("Password has been changed")
+                                .message("Password change successful")
+                                .build();
+        }
 }
--- server/src/main/java/vn/edu/actvn/server/repository/EnrollmentRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.actvn.server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import vn.edu.actvn.server.entity.Payment;
-
-@Repository
-public interface EnrollmentRepository extends JpaRepository<Payment, String> {
-}
